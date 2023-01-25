0b638a253d1ff6c1a9c37e3870dc402b00c7a3ee
pouyasadeqii <52070678+pouyasadeqii@users.noreply.github.com>
2023-01-26 00:07:45 +0330

    FEAT : add interceptors for response

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -2,6 +2,17 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
+axios.interceptors.response.use(
+  (res) => {
+    console.log(res);
+    return res;
+  },
+  (err) => {
+    console.log(err);
+    return Promise.reject();
+  }
+);
+
 const http = {
   get: axios.get,
   put: axios.put,
